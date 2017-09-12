c8962f8dbd9f39d00d57842dbfaebb3e1beca5ec
Joel Romero <joelrg1288@gmail.com>
2017-09-11 23:49:37 -0500

    Version final del main de la práctica, ya que se pide la n y el algoritmo como parametros al momento de la ejecución para poder realizar correctamente los scripts.

--- Prácticas/Práctica1/Ordenamiento.c
+++ Prácticas/Práctica1/Ordenamiento.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include "Funciones/funciones.c"
 
-////////////////////////////////////////////////////////////////////////////////////////
-////								Ordenamiento.c 									////
-////																				////
-////																				////
-//// Implementación de 6 algoritmos diferentes de ordenamiento de numeros, leyendo	////
-//// éstos de un archivo con un máximo de 10,000,000 de numeros introduciendo el 	////
-//// numero de numeros a ordenar y posteriormente seleccionar el algoritmo a usar,	////
-//// asimismo, se toman los tiempos de real y CPU de cada algoritmo para ver cual	////
-//// de los 6 es el algoritmo más eficiente.										////
-////																				////
-////																				////
-//// Autor: Romero Gamarra Joel Mauricio											////
-////////////////////////////////////////////////////////////////////////////////////////
-
 int main(int argc, char const *argv[])
 {
-	menu ();
+	int n, algoritmo;
+	if (argc < 3)
+		printf("Error, faltan argumentos.\n");
+	else
+	{
+		n = atoi (argv [1]);
+		algoritmo = atoi (argv [2]);
+	}
+	seleccionarAlgoritmo (algoritmo, n);
 	return 0;
 }
