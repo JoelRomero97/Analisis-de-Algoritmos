15c3a9d558146f367776981e8570f2e51ce9e99f
Joel Romero <joelrg1288@gmail.com>
2017-09-20 13:30:12 -0500

    Optimización de los algoritmos de busqueda lineal y busqueda binaria.

--- Prácticas/Práctica2/Funciones/funciones.c
+++ Prácticas/Práctica2/Funciones/funciones.c
@@ -50,48 +50,35 @@ int * LecturaArchivo (int * numeros, int n)
 
 void BusquedaLineal (int * numeros, int n, int NumeroBuscar)
 {
-	contador = 0;
 	uswtime (&utime0, &stime0, &wtime0);
-	for (i = 0; i < n; i ++)
-	{
-		if (numeros[i] == NumeroBuscar)
-		{
-			printf("Numero %d LOCALIZADO\n", NumeroBuscar);
-			contador ++;
-		}
-	}
-	if (contador == 0)
-			printf("Numero %d NO LOCALIZADO\n",NumeroBuscar);
+	for (i = 0; (i < n && numeros [i] != NumeroBuscar); i ++);
+	if (i >= n)
+		printf("Numero %d NO LOCALIZADO\n",NumeroBuscar);
+	else
+		printf("Numero %d LOCALIZADO\n", NumeroBuscar);
 	uswtime (&utime1, &stime1, &wtime1);
 	calculaTiempo (utime0, stime0, wtime0, utime1, stime1, wtime1, n, 1, NumeroBuscar);
 }
 
 void BusquedaBinaria (int * numeros, int n, int NumeroBuscar)
 {
-	int inferior = 0, superior = n, mitad;
+	int inferior = 0, superior = n, mitad = ((inferior + superior) / 2);
 	i = 0;
-	contador = 0;
 	uswtime (&utime0, &stime0, &wtime0);
-	while ((inferior <= superior) && (i < n))
+	while ((inferior <= superior) && (i < n) && (numeros [mitad] != NumeroBuscar))
 	{
 		mitad = ((inferior + superior) / 2);
-		if(numeros [mitad] == NumeroBuscar)
-		{
-			printf("Numero %d LOCALIZADO\n", NumeroBuscar);
-			contador ++;
-		}else if (numeros [mitad] > NumeroBuscar)
-		{
+		if (numeros [mitad] > NumeroBuscar)
 			superior = mitad;
-			mitad = ((inferior + superior) / 2);
-		}else
-		{
+		else
 			inferior = mitad;
-			mitad = ((inferior + superior) / 2);
-		}
+		mitad = ((inferior + superior) / 2);
 		i++;
 	}
-	if (contador == 0)
+	if (i >= n)
 		printf("Numero %d NO LOCALIZADO\n", NumeroBuscar);
+	else
+		printf("Numero %d LOCALIZADO\n", NumeroBuscar);
 	uswtime(&utime1, &stime1, &wtime1);
 	calculaTiempo (utime0, stime0, wtime0, utime1, stime1, wtime1, n, 2, NumeroBuscar);
 }
@@ -102,6 +89,11 @@ void BusquedaArbol (int * numeros, int n, int NumeroBuscar)
 	elemento e;
 	posicion pos;
 	InicializarArbol (&arbol);
+	FILE * numerosDesordenados;
+	numerosDesordenados = fopen ("numerosDesordenados.txt", "r");
+	for (i = 0; i < n; i ++)
+		fscanf (numerosDesordenados, "%d", &numeros [i]);
+	numeros [i] = '\0';
 	for (i = 0; i < n; i ++)
 	{
 		e.numero = numeros [i];
