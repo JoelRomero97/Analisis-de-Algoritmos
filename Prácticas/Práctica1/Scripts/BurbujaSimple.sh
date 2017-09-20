c37900efc8e8b42a3547de638b63542df5012d00
Joel Romero <joelrg1288@gmail.com>
2017-09-20 13:29:25 -0500

    Se agregan scripts para correr los programas con los distintos algoritmos.

--- Prácticas/Práctica1/Scripts/BurbujaSimple.sh
+++ Prácticas/Práctica1/Scripts/BurbujaSimple.sh
@@ -7,39 +7,21 @@ echo Ejecutando Algoritmo Burbuja Simple
 cd /home/joel/Escritorio
 gcc Ordenamiento.c -o Ordenamiento
 
+
 ./Ordenamiento 100 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 1000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 5000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 10000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 50000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 100000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 200000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 400000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 600000 1 >> AnalizadorTiempos.txt
-
 ./Ordenamiento 800000 1 >> AnalizadorTiempos.txt
+
 
 cd /home/joel/Escritorio/Scripts
 
 chmod 777 BurbujaOptimizada.sh
 
 ./BurbujaOptimizada.sh
-
-
-
-
-
-
-
-
-
-
-
